71c328464b6e49871e72b9994e9bb16c08f33c28
indzagi88@gmail.com <Indzagi19101989>
2020-06-15 02:19:58 +0300

    ДЗ 14 по уроку 16

---
+++ src/Strenght_fighters.java
@@ -0,0 +1,13 @@
+public abstract class Strenght_fighters extends Unit {
+    private int strength;
+
+
+    public int getStrength() {
+        return strength; 
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+}
